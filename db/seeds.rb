d73d4d4887db75b99fba6b1cdc5e611635b31c99
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-11-26 16:14:35 +0200

    Add controller for messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Message.destroy_all
-
 Message.create!([
   {
     greeting: 'Waramutse'
